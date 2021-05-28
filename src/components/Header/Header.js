932c35239f8f469dfc42c1a9facfa543a708a7e4
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-05-28 04:16:32 -0700

    add header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../../assets/Header/logo-dark.png';
+
+
+import './Header.scss'
+
+const Header = () => {
+    return (
+      <header className='header'>
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+        <ul>
+          <li>OUR COMPANY</li>
+          <li>LOCATIONS</li>
+          <li>CONTACT</li>
+        </ul>
+      </header>
+    );
+}
+export default Header;

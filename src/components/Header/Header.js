e250d2ca3c74fea6d8edb7cc8e998711467c096f
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-06-02 09:29:56 -0700

    add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,10 +7,10 @@ import logo from '../../assets/Header/logo-dark.png';
 const Header = () => {
     return (
       <header className='header'>
-        <div>
-          <img src={logo} alt="logo" />
+        <div className='logo'>
+          <img src={logo} alt="logo" height='35px'  />
         </div>
-        <ul>
+        <ul className='navbar'>
           <li>OUR COMPANY</li>
           <li>LOCATIONS</li>
           <li>CONTACT</li>

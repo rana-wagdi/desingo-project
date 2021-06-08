e9252326f3c7e50f1b11f3effb99f8ee7b512371
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-06-08 13:48:11 -0700

    add bootstrap

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,8 +5,8 @@ import hero from '../../assets/Home/bg-pattern-hero-home.svg'
 
 const Home = () => {
     return (
-      <section className="home">
-        <div className="home-title">
+      <section className="row home">
+        <div className="home-title  col-md-5 col-sm-6">
           <h1>Award-winning custom designs and digital branding solutions</h1>
           <p>
             With over 10 years in the industry, we are eexperiend inc reating
@@ -15,7 +15,7 @@ const Home = () => {
           </p>
           <button>LEARN MORE</button>
         </div>
-        <div className="img-mobile" >
+        <div className="img-mobile  col-md-3 col-sm-6">
           <img src={mobile} alt="mobile" />
         </div>
       </section>

ec1b148c4861a5dd48f0ece872081404a687a408
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-06-13 17:51:54 -0700

    style button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,13 +9,13 @@ const Home = () => {
         <div className="home-title ">
           <h1>Award-winning custom designs and digital branding solutions</h1>
           <p>
-            With over 10 years in the industry, we are eexperiend inc reating
+            With over 10 years in the industry, we are experienced in creating
             fully responsive websites, app design, and engaging brand
             experiences. Find out more about our services.
           </p>
           <button>LEARN MORE</button>
         </div>
-        <div className="img-mobile" >
+        <div className="img-mobile">
           <img src={mobile} alt="mobile" />
         </div>
       </section>

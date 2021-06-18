05e120646975ce7b3f40a7908a4e5f67cd8665b3
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-06-17 20:00:25 -0700

    add design

--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -6,29 +6,28 @@ const Main = () => {
     return (
       <section className="main">
         <div className="main-web">
-          {/* <div className="img-bg">
-            <img src={web} alt="web" />
-          </div> */}
-          <div className="over">
-            <div className="web-design">
-              <h1>WEB DESIGN</h1>
-              <p>
-                VIEW PROJECTS <IoIosArrowForward className="arrow-icon" />
-              </p>
-            </div>
+          <div className="web-design">
+            <h1>WEB DESIGN</h1>
+            <p>
+              VIEW PROJECTS <IoIosArrowForward className="arrow-icon" />
+            </p>
           </div>
         </div>
         <div className="main-app">
-          <h1>APP DESIGN</h1>
-          <p>
-            VIEW PROJECTS <IoIosArrowForward className="arrow-icon" />
-          </p>
+          <div className="app-design">
+            <h1>APP DESIGN</h1>
+            <p>
+              VIEW PROJECTS <IoIosArrowForward className="arrow-icon" />
+            </p>
+          </div>
         </div>
         <div className="main-graphic">
-          <h1>GRAPHIC DESIGN</h1>
-          <p>
-            VIEW PROJECTS <IoIosArrowForward className="arrow-icon" />
-          </p>
+          <div className="graphic-design">
+            <h1>GRAPHIC DESIGN</h1>
+            <p>
+              VIEW PROJECTS <IoIosArrowForward className="arrow-icon" />
+            </p>
+          </div>
         </div>
       </section>
     );

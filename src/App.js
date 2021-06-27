5863fbaee12b47a285b08e570c9a594dacf10817
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-06-26 23:04:43 -0700

    add section

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Main from './components/Home/Main';
 import PeopleContact from './components/Home/PeopleContact';
-
+import Footer from './components/Home/Footer';
 
 import './App.scss'
 function App() {
@@ -13,6 +13,7 @@ function App() {
     <Home />
     <Main />
     <PeopleContact />
+    <Footer />
     </div>
   );
 }
---
+++ src/components/Home/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+      <div className="talk">
+        <div>
+          <h1>Let’s talk about your project</h1>
+          <p>
+            Ready to take it to the next level? Contact us today and find out
+            how our expertise can help your business grow.
+          </p>
+        </div>
+        <div className="touch-button">
+            <button>GET IN TOUCH</button>
+        </div>
+      </div>
+    );
+}
+export default Footer;

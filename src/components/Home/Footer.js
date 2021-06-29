c8b2698dbc3c9daa8dc055bafd074c1738840773
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-06-28 22:14:27 -0700

    footer

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -1,26 +1,36 @@
 import React from 'react';
-
+import logo from "../../assets/Home/logo-light.png";
 
 const Footer = () => {
     return (
       <div>
-      <div className="talk">
-        <div>
-          <h1>Let’s talk about your project</h1>
-          <p>
-            Ready to take it to the next level? Contact us today and find out
-            how our expertise can help your business grow.
-          </p>
-        </div>
-        <div className="touch-button">
+        <div className="talk">
+          <div>
+            <h1>Let’s talk about your project</h1>
+            <p>
+              Ready to take it to the next level? Contact us today and find out
+              how our expertise can help your business grow.
+            </p>
+          </div>
+          <div className="touch-button">
             <button className="button">GET IN TOUCH</button>
+          </div>
         </div>
+        <footer className="footer">
+          <div className="nav-footer">
+            <div className="logo-footer">
+              <img src={logo} alt="logo" />
+            </div>
+            <hr className="line-logo" />
+            <ul className="link-footer">
+              <li>OUR COMPANY</li>
+              <li>LOCATION</li>
+              <li>CONTACT</li>
+            </ul>
+          </div>
+          <hr className="line" />
+        </footer>
       </div>
-      <footer className="footer">
-
-      </footer>
-      </div>
-      
     );
 }
 export default Footer;

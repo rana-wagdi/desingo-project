e261bf5c8708e3a915f6a8b2b323c4984490f713
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-07-03 03:20:16 -0700

    media

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import logo from "../../assets/Home/logo-light.png";
+import { AiFillFacebook } from "react-icons/ai";
+import { AiFillYoutube } from "react-icons/ai";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { FaPinterest } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+
 
 const Footer = () => {
     return (
@@ -29,6 +35,36 @@ const Footer = () => {
             </ul>
           </div>
           <hr className="line" />
+
+          <div className="contact-section">
+            <div>
+              <p>Designo Central Office</p>
+              <p>3886 Wellington Street</p>
+              <p>Toronto, Ontario M9C 3J5</p>
+            </div>
+            <div className="contactUs">
+              <p>Contact Us (Central Office)</p>
+              <p>P : +1 253-863-8967</p>
+              <p>M : contact@designo.co</p>
+            </div>
+            <div className="icon">
+              <span>
+                <AiFillFacebook  className="social-icon" size="30px" />
+              </span>
+              <span>
+                <AiFillYoutube className="social-icon" size="30px" />
+              </span>
+              <span>
+                <AiOutlineTwitter className="social-icon" size="30px" />
+              </span>
+              <span>
+                <FaPinterest className="social-icon" size="30px"/>
+              </span>
+              <span> 
+                <FiInstagram className="social-icon" size="30px"/>
+              </span>
+            </div>
+          </div>
         </footer>
       </div>
     );

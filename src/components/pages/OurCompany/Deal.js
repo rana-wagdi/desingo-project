841defa611fa0489037ec4a831bfeb51b5aff4f5
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-07-11 16:52:19 -0700

    add location

--- src/components/pages/OurCompany/Deal.js
+++ src/components/pages/OurCompany/Deal.js
@@ -8,17 +8,20 @@ const Deal = () => {
     return (
       <div className="deal">
         <div className="deal-location">
-          <div>
+          <div className="deal-country">
             <img src={canda} alt="canda" />
             <h2>CANADA</h2>
+            <button>SEE LOCATION</button>
           </div>
-          <div>
+          <div className="deal-country">
             <img src={australia} alt="australia" />
             <h2>AUSTRALIA</h2>
+            <button>SEE LOCATION</button>
           </div>
-          <div>
+          <div className="deal-country">
             <img src={unitedKingdom} alt="unitedKingdom" />
             <h2>UNITED KINGDOM</h2>
+            <button>SEE LOCATION</button>
           </div>
         </div>
       </div>

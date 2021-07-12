bed2861635ca6d473f79730cf0a0e481b73500ca
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-07-12 15:21:20 -0700

    deal

--- src/components/pages/OurCompany/Deal.js
+++ src/components/pages/OurCompany/Deal.js
@@ -2,7 +2,6 @@ import React from 'react';
 import canda from "../../../assets/pages/ourcompany/illustration-canada.svg";
 import australia from "../../../assets/pages/ourcompany/australia.svg";
 import unitedKingdom from "../../../assets/pages/ourcompany/illustration-united-kingdom.svg";
-//"../../../assets/pages/ourcompany/illustration-australia.svg";
 
 const Deal = () => {
     return (
@@ -24,6 +23,23 @@ const Deal = () => {
             <button>SEE LOCATION</button>
           </div>
         </div>
+
+        <section className="deal-section">
+          <div className="deal-text">
+            <h1>The real deal</h1>
+            <p>
+              As strategic partners in our clients’ businesses, we are ready to
+              take on any challenge as our own. Solving real problems require
+              empathy and collaboration, and we strive to bring a fresh
+              perspective to every opportunity. We make design and technology
+              more accessible and give you tools to measure success.
+            </p>
+            <p>
+            We are visual storytellers in appealing and captivating ways. By combining business and marketing strategies, we inspire audiences to take action and drive real results.
+            </p>
+          </div>
+          <div className="deal-img"></div>
+        </section>
       </div>
     );
 }

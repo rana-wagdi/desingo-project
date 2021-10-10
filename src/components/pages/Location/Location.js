ce33f494e8209d42606cf98c027e67b6dccd10b2
Rana Wagdi <73128713+rana2020701@users.noreply.github.com>
2021-10-10 11:38:52 +0200

    add canda

--- src/components/pages/Location/Location.js
+++ src/components/pages/Location/Location.js
@@ -12,21 +12,24 @@ const Location = ()=> {
                 </h1>
                 <div className="country__contant">
                 <div>
-                    <h2>Designo Central Office</h2>
+                    <h4>Designo Central Office</h4>
                     <p>3886 Wellington Street</p>
                     <p>Toronto, Ontario M9C 3J5</p>
                 </div>
                 <div>
-                    <h2>Contact</h2>
+                    <h4>Contact</h4>
                     <p>P : +1 253-863-8967</p>
                     <p>M : contact@desingo.co</p>
                 </div>
                 </div>
             </div>
             <div className="location__map">
-            <figure>
-            <img src={canda} alt="canda" />
-            </figure>
+            
+            <div className="img-location">
+
+            </div>
+           
+            
             </div>
         </div>
     )

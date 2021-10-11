e9b44599ac2c9180b12f5e5ade62a6be261091de
Rana Wagdi <73128713+rana2020701@users.noreply.github.com>
2021-10-11 02:53:10 +0200

    add country

--- src/components/pages/Location/Location.js
+++ src/components/pages/Location/Location.js
@@ -40,18 +40,18 @@ const Location = ()=> {
             </div>
             <div className="location__item__au">
                 <h1>
-                    Canada
+                    Australia
                 </h1>
                 <div className="country__contant__au">
                 <div>
-                    <h4>Designo Central Office</h4>
-                    <p>3886 Wellington Street</p>
-                    <p>Toronto, Ontario M9C 3J5</p>
+                    <h4>Designo AU Office</h4>
+                    <p>19 Balonne Street</p>
+                    <p>New South Wales 2443</p>
                 </div>
                 <div>
                     <h4>Contact</h4>
-                    <p>P : +1 253-863-8967</p>
-                    <p>M : contact@desingo.co</p>
+                    <p>P : (02) 67209092</p>
+                    <p>M : contact@desingo.au</p>
                 </div>
                 </div>
             </div>

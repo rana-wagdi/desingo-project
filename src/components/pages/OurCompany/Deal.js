4a6e53387d9d8020769a5c7030c791d411ce425b
Rana Wagdi <73128713+rana2020701@users.noreply.github.com>
2021-10-11 03:04:25 +0200

    add location

--- src/components/pages/OurCompany/Deal.js
+++ src/components/pages/OurCompany/Deal.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import canda from "../../../assets/pages/ourcompany/illustration-canada.svg";
 import australia from "../../../assets/pages/ourcompany/australia.svg";
 import unitedKingdom from "../../../assets/pages/ourcompany/illustration-united-kingdom.svg";
+
 
 const Deal = () => {
     return (
@@ -10,17 +12,23 @@ const Deal = () => {
           <div className="deal-country">
             <img src={canda} alt="canda" />
             <h2>CANADA</h2>
+            <Link to="/location">
             <button>SEE LOCATION</button>
+            </Link>
           </div>
           <div className="deal-country">
             <img src={australia} alt="australia" />
             <h2>AUSTRALIA</h2>
+            <Link to="/location">
             <button>SEE LOCATION</button>
+            </Link>
           </div>
           <div className="deal-country">
             <img src={unitedKingdom} alt="unitedKingdom" />
             <h2>UNITED KINGDOM</h2>
+            <Link to="/location">
             <button>SEE LOCATION</button>
+            </Link>
           </div>
         </div>
 

2b01058c55200115dc5c0851f7132e9bac91da84
Rana Wagdi <73128713+rana2020701@users.noreply.github.com>
2021-10-17 22:06:39 +0200

    contact

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Main from './components/Home/Main';
@@ -9,24 +9,12 @@ import OurCompany from "./components/pages/OurCompany/OurCompany";
 import Location from './components/pages/Location/Location';
 import Contact from './components/pages/contact/Contact';
 import MainFooter from './sharedComponent/mainFooter';
-import { useLocation } from 'react-router-dom';
+
 
 import './App.scss'
 
 const App =(props)=>{
-//  const LocationDisplay = () => {
-//     const location = useLocation()
-// let location = useLocation();
-// useEffect(
-//   () => {
-//     ga.send(['pageview', location.pathname])
-//   },
-//   [location]
-// )
-//     return <div>{location.pathname}</div>
-//   }
 
-    // const location = useLocation();
 
   return (
  
@@ -42,24 +30,13 @@ const App =(props)=>{
         <Route path="/ourCompany" exact component={OurCompany} />
         <Route path="/location" exact component={Location} />
         <Route path="/contact" exact component={Contact} />
-{/*       
-        {props.location.pathname !== '/contact' && <Footer /> } */}
-     
-        {/* {location.pathname !== '/user' && <Footer /> } */}
-        {/* {props.location.pathname !== '/contact' && <Footer /> } */}
-     
-       {/* {window.location.pathname === "/contact" ? null : <Footer />} */}
-        {/* <Route path="/contact" exact component={Contact} />
-        <Footer /> */} 
     
      
         <Footer />
         
       </div>
-      {/* <Switch>
-      <Route path="/contact" exact component={Contact} />
-      {location.pathname !== '/contact' && <Footer /> }
-      </Switch> */}
+
+
      
     </Router>
     
--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -12,12 +12,12 @@ const Contact = () => {
             <p>Ready to take the next level? Let's talk about your project or idea and find out how we can help youe business grow. If you are looking for unique digital experiences that's relatable to your user, drop us a line</p>
         </div>
         <div className="contact__content__inputs">
-            <label >
-                <input type="text" placeholder="Name" /><br />
+            <label id="contact-form">
+                <input type="text" placeholder="Name"  required="required" pattern="[A-Za-z]{1,20}"/><br />
                 <input type="email" placeholder="Email Adress" /><br />
                 <input type="text" placeholder="Phone" /><br />
-                <textarea id="" name="" rows="4" cols="50"></textarea><br />
-                <button>Submit</button>
+                <textarea placeholder="Your Message" name="" rows="4" cols="50"></textarea><br />
+                <button type="submit" form="contact-form">Submit</button>
             </label>
         </div>
         </div>
